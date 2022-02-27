09661c97423bdc278513cd11af8afc622326f063
ericgaspar <junk.eg@free.fr>
2022-02-27 18:44:56 +0100

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,7 +4,7 @@
 # COMMON VARIABLES
 #=================================================
 
-YNH_PHP_VERSION="7.3"
+YNH_PHP_VERSION="8.0"
 
 pkg_dependencies="php${YNH_PHP_VERSION}-tokenizer php${YNH_PHP_VERSION}-pdo php${YNH_PHP_VERSION}-bcmath php${YNH_PHP_VERSION}-ctype php${YNH_PHP_VERSION}-fileinfo php${YNH_PHP_VERSION}-json php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-mbstring"
 
